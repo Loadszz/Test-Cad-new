3a9e7b6b7417bad42acc3b2dbd10e607381795b7
Loadszz <66636253+Loadszz@users.noreply.github.com>
2024-12-05 16:39:42 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,24 +13,24 @@ module.exports = {
 		// },
 		extend: {
 			screens: {
-				'pc': { 'max': '1710px' },
-				'lpl': { 'max': '1440px' },
-				'tb': { 'max': '1024px' },
-				'mb': { 'max': '768px' },
-				'mm': { 'max': '425px' },
+				pc: { max: '1710px' },
+				lpl: { max: '1440px' },
+				tb: { max: '1024px' },
+				mb: { max: '768px' },
+				mm: { max: '425px' },
 			},
 			fontFamily: {
 				raleway: ['var(--font-raleway)'],
 				unbounded: ['var(--font-unbounded)'],
 			},
 			backgroundImage: {
-				'hero': "url('/images/hero/hero-background.jpg')",
-				'hero2': "url('/images/hero/hero-background-2.jpg')",
-				'hero3': "url('/images/hero/hero-background-3.jpg')",
-				'burger': "url('/images/header/burger-background.svg')",
-				'footer': "url('/images/footer/footer-background.svg')",
+				hero: "url('/images/hero/hero-background.jpg')",
+				hero2: "url('/images/hero/hero-background-2.jpg')",
+				hero3: "url('/images/hero/hero-background-3.jpg')",
+				burger: "url('/images/header/burger-background.svg')",
+				footer: "url('/images/footer/footer-background.svg')",
 				'our-client': "url('/images/our-client/our-client-bg.jpg')",
-				'contact': "url('/images/contact/contact-background.jpg')"
+				contact: "url('/images/contact/contact-background.jpg')",
 			},
 			colors: {
 				white: '#FFFFFF',
@@ -38,7 +38,11 @@ module.exports = {
 				bdGray: '#E6E6E6',
 				accent: '#37C0F6',
 				myBrown: 'rgba(255, 255, 255, 0.20)',
-				footerRgb: 'rgba(255, 255, 255, 0.12)'
+				footerRgb: 'rgba(255, 255, 255, 0.12)',
+			},
+			boxShadow: {
+				'custom-combined':
+					'0 14px 30px 0 rgba(157, 167, 170, 0.08), 0 4px 8px 0 rgba(157, 167, 170, 0.16)',
 			},
 		},
 	},
